915fa5f706317be731a6cba3b72532a800f0f766
jerrybennett <jerrysbusiness@gmail.com>
2017-12-21 19:01:19 -0500

    passed all tests

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -3,6 +3,7 @@ class AttractionsController < ApplicationController
 
   def index
     @attractions = Attraction.all
+    @user = current_user
   end
 
   def show
@@ -27,13 +28,15 @@ class AttractionsController < ApplicationController
   end
 
   def update
-
+    @attraction = Attraction.find(params[:id])
+    @attraction.update(attraction_params)
+    redirect_to @attraction
   end
 
   private
 
   def attraction_params
-    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :min_height)
+    params.require(:attraction).permit(:name, :tickets, :nausea_rating, :min_height, :happiness_rating)
   end
 
   def set_attraction
